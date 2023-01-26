933ff521df44113afd261c436e177773ba007b1c
Daniel Curtis <dmcurtis05@gmail.com>
2023-01-26 09:42:35 -0600

    Reorganized files

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.settings.config import settings
-from app.models import Base
+from app.models.sa_models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
