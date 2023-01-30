8bea1c403e2a61e00886896c8dbbb1b683af06f1
Daniel Curtis <dmcurtis05@gmail.com>
2023-01-29 21:20:26 -0600

    Added username to user table

--- app/models/sa_models.py
+++ app/models/sa_models.py
@@ -9,6 +9,7 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, nullable=False)
     admin = Column(Boolean, nullable=False, server_default=text('False'))
+    username = Column(String, nullable=False, unique=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
