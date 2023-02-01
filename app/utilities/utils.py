bc6ea86e222c875d6389e596c33cd3225d4daae6
Daniel Curtis <dmcurtis05@gmail.com>
2023-01-31 21:22:54 -0600

    Added row to dict

--- app/utilities/utils.py
+++ app/utilities/utils.py
@@ -10,3 +10,11 @@ def hash(data: str):
 
 def verify(plain_pwd, hashed_pwd):
     return pwd_context.verify(plain_pwd, hashed_pwd)
+
+def row_to_dict(row):
+
+    converted_dict = {}
+    for column in row.__table__.columns:
+        converted_dict[column.name] = getattr(row, column.name)
+
+    return converted_dict
