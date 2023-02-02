532a25842efa065cb402e16e4c37d6b1d446fee1
Daniel Curtis <dmcurtis05@gmail.com>
2023-02-02 11:54:51 -0600

    Removed load all route

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -114,13 +114,4 @@ class CompletedExercisesOut(CompletedExercises):
     class Config:
         orm_mode = True
 
-class LoadAll(BaseModel):
-    
-    exercises: List[ExerciseOut]
-    resources: List[ResourceOut]
-    prompts: List[PromptOut]
-    completed_exercises: CompletedExercisesOut
-
-    class Config:
-        orm_mode = True
 
