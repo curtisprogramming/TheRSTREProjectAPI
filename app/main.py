4a6237a4cb1f220fc825d9aefedd36a05d7967f8
Daniel Curtis <dmcurtis05@gmail.com>
2023-02-02 12:07:27 -0600

    Added load all route

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 #posts, users, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import prompts, exercises, login, resources, users, journalEntries, completed_exercises
+from .routers import prompts, exercises, login, resources, users, journalEntries, completed_exercises, loadAll
 
 
 app = FastAPI()
-
 origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -15,7 +13,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 #adds routes to app
 app.include_router(users.router)
 app.include_router(resources.router)
@@ -24,6 +21,7 @@ app.include_router(login.router)
 app.include_router(prompts.router)
 app.include_router(journalEntries.router)
 app.include_router(completed_exercises.router)
+app.include_router(loadAll.router)
 
 @app.get("/")
 def root():
--- app/models/schemas.py
+++ app/models/schemas.py
@@ -114,4 +114,12 @@ class CompletedExercisesOut(CompletedExercises):
     class Config:
         orm_mode = True
 
+class LoadAll(BaseModel):
 
+    exercises: List[ExerciseOut]
+    resources: List[ResourceOut]
+    prompts: List[PromptOut]
+    completed_exercises: CompletedExercisesOut
+
+    class Config:
+        orm_mode = True
