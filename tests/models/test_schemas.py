b326fbcf15727de56574499f8fbb0f7632d769f6
Daniel Curtis <dmcurtis05@gmail.com>
2023-03-23 15:07:27 -0500

    Added UserBase and UserOut tests

--- tests/models/test_schemas.py
+++ tests/models/test_schemas.py
@@ -1,12 +1,7 @@
 from app.models import schemas
 from pytest import raises
-from pydantic import ValidationError
+from pydantic import ValidationError, BaseModel
 from json import loads
-
-
-
-
-
 
 #EXERCISES
 #EXERCISE BASE
@@ -57,6 +52,9 @@ def test_exercise_base_no_image_name():
 
     assert raises(ValidationError)
     assert error_dict == [{'loc': ['image_name'], 'msg': 'field required', 'type': 'value_error.missing'}]
+
+def test_exercise_base_extends_BaseModel():
+    assert issubclass(schemas.Exercise.ExerciseBase, BaseModel)
 
 #EXERCISE OUT
 #############################################################################################################
@@ -183,6 +181,9 @@ def test_resource_base_no_call():
     assert raises(ValidationError)
     assert error_dict == [{'loc': ['call'], 'msg': 'field required', 'type': 'value_error.missing'}]
 
+def test_resource_base_extends_BaseModel():
+    assert issubclass(schemas.Resource.ResourceBase, BaseModel)
+
 #RESOURCE OUT
 #####################################################################################################################
 def test_resource_out_correct():
@@ -243,7 +244,10 @@ def test_prompt_base_no_prompt():
     assert raises(ValidationError)
     assert error_dict == [{'loc': ['prompt'], 'msg': 'field required', 'type': 'value_error.missing'}]
 
-#RESOURCE OUT
+def test_resource_base_extends_BaseModel():
+    assert issubclass(schemas.Prompt.PromptOut, BaseModel)
+
+#PROMPT OUT
 #####################################################################################################################
 def test_prompt_out_correct():
     data = {"id": 100,"prompt": "This is a prompt"}
@@ -273,3 +277,145 @@ def test_prompt_out_extends_prompt_base():
 def test_prompt_out_orm_mode():
 
     assert schemas.Prompt.PromptOut.Config.orm_mode == True
+
+
+
+
+
+
+#USERS
+#USER DATA
+#USER BASE
+#############################################################################################################################
+def test_user_base_correct():
+    data = {"email": "example@gmail.com", "username": "test_username", "password": "test_password", "phone_number": "(123) 456-7890"}
+
+    user = schemas.UserData.UserBase(**data)
+    print(user.dict())
+
+    assert user.dict() == data
+
+def test_user_base_no_email():
+    data = {"username": "test_username", "password": "test_password", "phone_number": "(123) 456-7890"}
+
+    try:
+         schemas.UserData.UserBase(**data)
+    except ValidationError as err:
+        error_dict = loads(err.json())
+        print(error_dict)
+    
+
+    assert raises(ValidationError)
+    assert error_dict == [{'loc': ['email'], 'msg': 'field required', 'type': 'value_error.missing'}]
+
+def test_user_base_no_username():
+    data = {"email": "example@gmail.com", "password": "test_password", "phone_number": "(123) 456-7890"}
+
+    try:
+         schemas.UserData.UserBase(**data)
+    except ValidationError as err:
+        error_dict = loads(err.json())
+        print(error_dict)
+    
+
+    assert raises(ValidationError)
+    assert error_dict == [{'loc': ['username'], 'msg': 'field required', 'type': 'value_error.missing'}]
+
+def test_user_base_no_password():
+    data = {"email": "example@gmail.com", "username": "test_username", "phone_number": "(123) 456-7890"}
+
+    try:
+         schemas.UserData.UserBase(**data)
+    except ValidationError as err:
+        error_dict = loads(err.json())
+        print(error_dict)
+    
+
+    assert raises(ValidationError)
+    assert error_dict == [{'loc': ['password'], 'msg': 'field required', 'type': 'value_error.missing'}]
+
+def test_user_base_no_phone_number():
+    data = {"email": "example@gmail.com", "username": "test_username", "password": "test_password"}
+
+    user = schemas.UserData.UserBase(**data)
+    print(user)
+
+    data['phone_number'] = None
+    assert user.dict() == data
+
+def test_user_base_invalid_email():
+    data = {"email": "examplegmail.com", "username": "test_username", "password": "test_password", "phone_number": "(123) 456-7890"}
+
+    try:
+        schemas.UserData.UserBase(**data)
+    except ValidationError as err:
+        error_dict = loads(err.json())
+        print(error_dict)
+
+    assert raises(ValidationError)
+    assert error_dict == [{'loc': ['email'], 'msg': 'value is not a valid email address', 'type': 'value_error.email'}]
+
+
+
+def test_user_base_extends_BaseModel():
+    assert issubclass(schemas.UserData.UserBase, BaseModel)
+
+#USER OUT
+###################################################################################################################################
+def test_user_out_correct():
+    data = {"id": 100, "created_at": "2023-03-23T17:20:10.576526+00:00","email": "example@gmail.com", "username": "test_username", "password": "test_password", "phone_number": "(123) 456-7890"}
+
+    user = schemas.UserData.UserOut(**data)
+    user_dict = user.dict()
+    user_dict['created_at'] = user.created_at.isoformat()
+    print(user_dict)
+    assert user_dict == {"id": 100, "created_at": "2023-03-23T17:20:10.576526+00:00","email": "example@gmail.com", "username": "test_username", "phone_number": "(123) 456-7890"}
+
+
+def test_user_out_no_id():
+    data = {"created_at": "2023-03-23T17:20:10.576526+00:00","email": "example@gmail.com", "username": "test_username", "password": "test_password", "phone_number": "(123) 456-7890"}
+
+    try:
+        schemas.UserData.UserOut(**data)
+    except ValidationError as err:
+        error_dict = loads(err.json())
+        print(error_dict)
+    
+
+    assert raises(ValidationError)
+    assert error_dict == [{'loc': ['id'], 'msg': 'field required', 'type': 'value_error.missing'}]
+
+def test_user_out_no_created_at():
+    data = {"id": 100, "email": "example@gmail.com", "username": "test_username", "password": "test_password", "phone_number": "(123) 456-7890"}
+
+    try:
+        schemas.UserData.UserOut(**data)
+    except ValidationError as err:
+        error_dict = loads(err.json())
+        print(error_dict)
+    
+
+    assert raises(ValidationError)
+    assert error_dict == [{'loc': ['created_at'], 'msg': 'field required', 'type': 'value_error.missing'}]
+
+def test_user_out_invalid_created_at_date():
+    data = {"id": 100, "created_at": "2023-03-23","email": "example@gmail.com", "username": "test_username", "password": "test_password", "phone_number": "(123) 456-7890"}
+
+    try:
+        schemas.UserData.UserOut(**data)
+    except ValidationError as err:
+        error_dict = loads(err.json())
+        print(error_dict)
+    
+
+    assert raises(ValidationError)
+    assert error_dict == [{'loc': ['created_at'], 'msg': 'invalid datetime format', 'type': 'value_error.datetime'}]
+
+
+def test_user_out_extends_prompt_base():
+    
+    assert issubclass(schemas.UserData.UserOut, schemas.UserData.UserBase)
+
+def test_user_out_orm_mode():
+
+    assert schemas.UserData.UserOut.Config.orm_mode == True
