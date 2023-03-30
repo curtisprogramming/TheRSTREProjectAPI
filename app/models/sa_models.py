b41bda4c03ee04701d0b5f29f8f79dd880ca682e
Daniel Curtis <curt.programming@gmail.com>
2023-03-30 12:30:31 -0500

    Cleanup

--- app/models/sa_models.py
+++ app/models/sa_models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from ..database import Base
