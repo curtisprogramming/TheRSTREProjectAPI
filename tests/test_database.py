d618529bd003eec1d26237c6f913df8e9029aa5c
Daniel Curtis <curt.programming@gmail.com>
2023-04-10 09:29:39 -0500

    Removed autocommit test

--- tests/test_database.py
+++ tests/test_database.py
@@ -6,11 +6,6 @@ def test_database_url():
     correct_url = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
     assert database.SQLALCHEMY_DATABASE_URL == correct_url
 
-def test_db_autocommit():
-    correct_value = False
-    session = database.get_db()
-    assert session.__next__().autocommit == correct_value
-
 def test_db_autoflush():
     correct_value = False
     session = database.get_db()
